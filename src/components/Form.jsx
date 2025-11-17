65b2fdac7577d075f1133e3eb00d74eb06a4d306
Andrius <andrius88@gmail.com>
2025-11-17 11:29:35 +0200

    React template project added

---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+function Form() {
+  return (
+    <form className="p-4 bg-blue-400">
+      <input
+        className="bg-white"
+        type="text"
+        name=""
+        id=""
+      />
+    </form>
+  );
+}
+
+export default Form;
